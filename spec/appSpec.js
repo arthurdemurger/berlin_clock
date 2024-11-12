8a59bd7abd7fe65c3e1650fb0e02cf8f4016f5e6
lirockk <98538729+lirockk@users.noreply.github.com>
2024-11-12 23:09:06 +0100

    add: five_minutes_blocks case 46

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -166,4 +166,10 @@ describe("Berlin Clock function", function() {
 
 		expect(result).toBe("JJRJJRJJROO");
 	});
+
+	it("five_minutes_blocks should return JJRJJRJJROO when given 46", function() {
+		let result = berlin_clock.five_minutes_blocks(46);
+
+		expect(result).toBe("JJRJJRJJROO");
+	});
 });
