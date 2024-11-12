04a076b0b53a662d268f987040f7d7dabd65f632
lirockk <98538729+lirockk@users.noreply.github.com>
2024-11-12 16:22:47 +0100

    add: case 0

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,6 +1,11 @@
 import { BerlinClock } from '../src/app.js';
 
 describe("Berlin Clock function", function() {
-	var berlin_clock = new BerlinClock();
+	let berlin_clock = new BerlinClock();
 
+	it("should return ", function() {
+		let result = berlin_clock.convert_to_berlin_time(0);
+
+		expect(result).toBe("O\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO");
+	});
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 export class BerlinClock {
 
 	convert_to_berlin_time(timestamp) {
-		return ("");
-	}
+		let berlin_time = "O\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO";
 
+		return (berlin_time);
+	}
 }
