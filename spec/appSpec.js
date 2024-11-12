bd6b7537e54866b7dab0f259a0c47edaa2aa58b9
julienhanse <shibaz.contact@gmail.com>
2024-11-12 17:52:12 +0100

    add: case 0

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,70 +3,9 @@ import { BerlinClock } from '../src/app.js';
 describe("Berlin Clock function", function() {
 	let berlin_clock = new BerlinClock();
 
-	it("simples_minutes should return OOOO when given 0", function() {
-		let result = berlin_clock.simples_minutes(0);
-
-		expect(result).toBe("OOOO");
+	it("should return even when the seconds are 0", function() {
+		let result = berlin_clock.seconds(0);
+	
+		expect(result).toBe("R");
 	});
-
-	it("simples_minutes should return JOOO when given 1", function() {
-		let result = berlin_clock.simples_minutes(1);
-
-		expect(result).toBe("JOOO");
-	});
-
-	it("simples_minutes should return JJOO when given 2", function() {
-		let result = berlin_clock.simples_minutes(2);
-
-		expect(result).toBe("JJOO");
-	});
-
-	it("simples_minutes should return JJJO when given 3", function() {
-		let result = berlin_clock.simples_minutes(3);
-
-		expect(result).toBe("JJJO");
-	});
-
-	it("simples_minutes should return JJJJ when given 4", function() {
-		let result = berlin_clock.simples_minutes(4);
-
-		expect(result).toBe("JJJJ");
-	});
-
-	it("simples_minutes should return OOOO when given 5", function() {
-		let result = berlin_clock.simples_minutes(5);
-
-		expect(result).toBe("OOOO");
-	});
-
-	it("simples_minutes should return JOOO when given 6", function() {
-		let result = berlin_clock.simples_minutes(6);
-
-		expect(result).toBe("JOOO");
-	});
-
-	it("five_minutes_blocks should return OOOOOOOOOOO when given 0", function() {
-		let result = berlin_clock.five_minutes_blocks(0);
-
-		expect(result).toBe("OOOOOOOOOOO");
-	});
-
-	it("five_minutes_blocks should return OOOOOOOOOOO when given 1", function() {
-		let result = berlin_clock.five_minutes_blocks(1);
-
-		expect(result).toBe("OOOOOOOOOOO");
-	});
-
-	it("five_minutes_blocks should return JOOOOOOOOOO when given 5", function() {
-		let result = berlin_clock.five_minutes_blocks(5);
-
-		expect(result).toBe("JOOOOOOOOOO");
-	});
-
-	it("five_minutes_blocks should return JOOOOOOOOOO when given 6", function() {
-		let result = berlin_clock.five_minutes_blocks(6);
-
-		expect(result).toBe("JOOOOOOOOOO");
-	});
-
 });
--- src/app.js
+++ src/app.js
@@ -1,43 +1,5 @@
 export class BerlinClock {
-
-	convert_to_berlin_time(timestamp) {
-		let berlin_time = "O\nOOOO\nOOOO\nOOOOOOOOOOO\n";
-
-		berlin_time += this.simples_minutes(timestamp);
-
-		return (berlin_time);
-	}
-
-	five_minutes_blocks(time_in_minutes) {
-		if (time_in_minutes >= 5) {
-			return ("JOOOOOOOOOO");
-		}
-
-		return ("OOOOOOOOOOO");
-	}
-
-
-	simples_minutes(time_in_minutes) {
-		const minutes = time_in_minutes % 5;
-
-		if (minutes === 0) {
-			return ("OOOO");
-		}
-
-		if (minutes === 1) {
-			return ("JOOO");
-		}
-
-		if (minutes === 2) {
-			return ("JJOO");
-		}
-
-		if (minutes === 3) {
-			return ("JJJO");
-		}
-
-		if (minutes === 4) {
-			return ("JJJJ");
-		}
+	seconds(time_in_seconds) {
+		return time_in_seconds % 2 === 0 ? "R" : "O";
 	}
 }
